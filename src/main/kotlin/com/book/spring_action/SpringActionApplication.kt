b66916b7cdc38f00f73ce3c6907c229f2a6cad12
sukwoojung <ggp03016@naver.com>
2023-01-25 23:54:03 +0900

    first commit

---
+++ src/main/kotlin/com/book/spring_action/SpringActionApplication.kt
@@ -0,0 +1,11 @@
+package com.book.spring_action
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringActionApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringActionApplication>(*args)
+}
